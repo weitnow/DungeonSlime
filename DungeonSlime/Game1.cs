1057a60ea0b675223ff3b441f92c913a93bbd14c
Christian Keusch <weitnow@gmail.com>
2025-08-26 17:18:59 +0200

    ended09

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -10,11 +10,11 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
-    // texture region that defines the slime sprite in the atlas.
-    private TextureRegion _slime;
+    // Defines the slime animated sprite.
+    private AnimatedSprite _slime;
 
-    // texture region that defines the bat sprite in the atlas.
-    private TextureRegion _bat;
+    // Defines the bat animated sprite.
+    private AnimatedSprite _bat;
 
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
@@ -33,11 +33,13 @@ public class Game1 : Core
         // Create the texture atlas from the XML configuration file
         TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
 
-        // retrieve the slime region from the atlas.
-        _slime = atlas.GetRegion("slime");
+        // Create the slime animated sprite from the atlas.
+        _slime = atlas.CreateAnimatedSprite("slime-animation");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
 
-        // retrieve the bat region from the atlas.
-        _bat = atlas.GetRegion("bat");
+        // Create the bat animated sprite from the atlas.
+        _bat = atlas.CreateAnimatedSprite("bat-animation");
+        _bat.Scale = new Vector2(4.0f, 4.0f);
 
 
     }
@@ -47,7 +49,11 @@ public class Game1 : Core
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        // Update the slime animated sprite.
+        _slime.Update(gameTime);
+
+        // Update the bat animated sprite.
+        _bat.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -60,11 +66,11 @@ public class Game1 : Core
         // Begin the sprite batch to prepare for rendering.
         SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
-        // Draw the slime texture region at a scale of 4.0
-        _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
+        // Draw the slime sprite.
+        _slime.Draw(SpriteBatch, Vector2.One);
 
-        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
-        _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
+        // Draw the bat sprite 10px to the right of the slime.
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
 
 
 
