dd9feb199f64d067f5b4effc816e68fe6d19a006
Christian <weitnow@gmail.com>
2025-10-17 17:02:57 +0200

    gum stylized

--- MonoGameLibrary/Graphics/TextureRegion.cs
+++ MonoGameLibrary/Graphics/TextureRegion.cs
@@ -10,6 +10,26 @@ namespace MonoGameLibrary.Graphics;
 public class TextureRegion
 {
     /// <summary>
+    /// Gets the top normalized texture coordinate of this region.
+    /// </summary>
+    public float TopTextureCoordinate => SourceRectangle.Top / (float)Texture.Height;
+
+    /// <summary>
+    /// Gets the bottom normalized texture coordinate of this region.
+    /// </summary>
+    public float BottomTextureCoordinate => SourceRectangle.Bottom / (float)Texture.Height;
+
+    /// <summary>
+    ///  Gets the left normalized texture coordinate of this region.
+    /// </summary>
+    public float LeftTextureCoordinate => SourceRectangle.Left / (float)Texture.Width;
+
+    /// <summary>
+    /// Gets the right normalized texture coordinate of this region.
+    /// </summary>
+    public float RightTextureCoordinate => SourceRectangle.Right / (float)Texture.Width;
+
+    /// <summary>
     /// Gets or Sets the source texture this texture region is part of.
     /// </summary>
     public Texture2D Texture { get; set; }
